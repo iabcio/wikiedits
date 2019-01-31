198e520cbb65ab64db539781410c2cedd1d6f3dd
hesc <hesc@guahao.com>
2019-01-31 10:09:36 +0800

    shuchen : kafka

--- src/main/java/io/iabc/WikipediaAnalysis.java
+++ src/main/java/io/iabc/WikipediaAnalysis.java
@@ -17,12 +17,15 @@
 package io.iabc;
 
 import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
 
@@ -58,7 +61,12 @@ public class WikipediaAnalysis {
                 }
             });
 
-        result.print();
+        result.map(new MapFunction<Tuple2<String, Long>, String>() {
+            @Override
+            public String map(Tuple2<String, Long> tuple) {
+                return tuple.toString();
+            }
+        }).addSink(new FlinkKafkaProducer011<>("localhost:9092", "wiki-result", new SimpleStringSchema()));
 
         see.execute();
     }
